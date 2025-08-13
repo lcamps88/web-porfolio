0b952ef25df4f7988eec561e648f5818d03758a7
Lisandra <lisandra.camps@gmail.com>
2025-08-13 11:56:58 -0400

    new projects

--- components/Work.jsx
+++ components/Work.jsx
@@ -4,9 +4,11 @@ import Project1 from '@/public/images/diosef.org.webp'
 import Project2 from '@/public/images/nocostshoes.webp'
 import Project3 from '@/public/images/realestate4all.webp'
 import Project4 from '@/public/images/schmutzes.webp'
-import Project5 from '@/public/images/pink-zebra-store-locator.vercel.app.webp'
+import Project5 from '@/public/images/darksaberlabs.png'
 import Project6 from '@/public/images/sann-kalla.webp'
 import Project7 from '@/public/images/ce-restaurants1.png'
+import Project8 from '@/public/images/sorite.png'
+import Project9 from '@/public/images/atlanticsignal.png'
 
 const Work = () => {
   return (
@@ -41,10 +43,24 @@ const Work = () => {
                 color='border-pink_card'
               />
               <ProjectsCard
+                title='Atlantic Signal'
+                image={Project9}
+                link='https://atlanticsignal.com/'
+                category='WordPress/Elementor'
+                color='border-pink_card'
+              />
+              <ProjectsCard
                 title='Schmutzes'
                 image={Project4}
                 link='https://schmutzes.com/'
                 category='Shopify'
+                color='border-red_card'
+              />
+               <ProjectsCard
+                title='SoRite'
+                image={Project8}
+                link='https://sorite.com/'
+                category='Custom Liquid Theme / Shopify'
                 color='border-red_card'
               />
                 <ProjectsCard
@@ -62,10 +78,10 @@ const Work = () => {
                 color='border-green_card'
               />
               <ProjectsCard
-                title='Store Location'
+                title='Dark Saber Labs'
                 image={Project5}
-                link='https://pink-zebra-store-locator.vercel.app/'
-                category='Mapbox'
+                link='https://darksaberlabs.com/'
+                category='Nextjs /Custom Development'
                 color='border-blue_card'
               />
               
